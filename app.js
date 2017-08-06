15a9ce5e1a1b76bdd5c12750ab0d10e12a482ced
Dhaval Chauhan <dhaval@dr4cun0.com>
2017-08-06 12:13:56 -0400

    Misc

--- app.js
+++ app.js
@@ -3,21 +3,54 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// prepare server
-//app.use('/api', api); // redirect API calls
-app.use('/', express.static(__dirname + '/app.html')); // redirect root
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/pagediff');
+
+
+var log = mongoose.model('log',
+	{
+		url: String,
+		initial-c-length: Number,
+		last-c-length: Number,
+		current-c-length: Number,
+		update: Boolean
+	}
+);
+
+
+// static pages
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstra
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts')); // redirect CSS bootstrap
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/app.html'));
 });
 
-
 app.post('/add', function(req, res) {
+	// Adding URL to DB
+
+	var logger = new log({ url: req.body.url, update: 0 });
+
+	logger.save(function (err) {
+	  if (err) {
+	    console.log(err);
+	  } else {
+	    console.log('meow');
+	  }
+	});
+});
+
+app.get('/notifcations', function(req, res) {
 	//
 });
+
+app.get('/history', function(req, res) {
+	//
+});
+
 app.listen(4040, function () {
   //console.log('Example app listening on port 4040!')
 })
